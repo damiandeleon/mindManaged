38287b4f422e81537641879ed75c0c3041879e48
caroline-e-miller <caroline.miller@ymail.com>
2021-05-24 17:28:56 -0500

    added pages and API util

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Navbar from "./components/Navbar/Nav";
+import Home from "./pages/Home";
+import Journal from "./pages/Journal";
+import Medication from "./pages/Medication";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+// import logo from './logo.svg';
+// import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/Journal" component={Journal} />
+        <Route exact path="/Medication" component={Medication} />
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Navbar/Nav.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Nav.css";
+
+// Depending on the current path, this component sets the "active" class on the appropriate navigation link item
+function Navbar() {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <a className="navbar-brand" href="/">Mind Managed</a>
+        </nav>
+    );
+}
+
+export default Navbar;
