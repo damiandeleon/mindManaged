b15fa7a88009ea5fe4c50991e80f6ac01aada8f9
caroline-e-miller <caroline.miller@ymail.com>
2021-05-24 19:53:20 -0500

    deleted unnecessary function

--- src/pages/Medication.js
+++ src/pages/Medication.js
@@ -21,17 +21,6 @@ class Search extends Component {
         this.setState({ search: event.target.value });
     };
 
-    handleFormSubmit = event => {
-        event.preventDefault();
-        API.getDrugInfo(this.state.search)
-            .then(res => {
-                if (res.data.status === "error") {
-                    throw new Error(res.data.message);
-                }
-                this.setState({ results: res.data.message, error: "" });
-            })
-            .catch(err => this.setState({ error: err.message }));
-    };
     render() {
         return (
             <div>
