39766c9c98c85caa38e0fa1dfbd0e81b78a6bb41
caroline-e-miller <caroline.miller@ymail.com>
2021-05-26 16:47:24 -0500

    Created card skeleton, drug interaction link

--- src/pages/Medication.js
+++ src/pages/Medication.js
@@ -6,21 +6,40 @@ import API from "../utils/API";
 class Search extends Component {
     state = {
         search: "",
-        prescription: [],
+        prescriptions: [],
         results: [],
         error: ""
     };
 
-    // When the component mounts, get a list of all available base breeds and update this.state.breeds
     componentDidMount() {
-        API.getDrugInfo()
-            .then(res => this.setState({ prescription: res.data.message }))
+        API.getDrugInfo(this.state.search)
+            .then(res => this.setState({ prescriptions: res.data.results }))
             .catch(err => console.log(err));
     }
 
     handleInputChange = event => {
-        this.setState({ search: event.target.value });
+        console.log('rx');
+        const value = event.target.value;
+        const results = this.state.prescriptions;
+
+        console.log(results);
+        const searchResults = results.filter((result) => {
+            console.log(result.name.first);
+            // toLowerCase
+            return result.name.first.toLowerCase().startsWith(this.state.search)
+        });
+        console.log(searchResults);
+
+        this.setState({
+            search: value,
+            prescriptions: searchResults
+        });
     };
+
+    // handleFormSubmit = event => {
+    //     event.preventDefault();
+    //     this.getDrugInfo(this.state.search);
+    // };
 
     render() {
         return (
@@ -28,6 +47,7 @@ class Search extends Component {
                 <h1 className="text-center">Find your medication here</h1>
                 <SearchForm
                     handleInputChange={this.handleInputChange}
+                    // handleFormSubmit={this.handleFormSubmit}
                     prescriptions={this.state.prescriptions}
                     search={this.state.search}
                 />
