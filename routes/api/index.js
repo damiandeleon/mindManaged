8b7965ef2e0f001abd1be247902149a4048e70e8
Vanity <ktkyletran@gmail.com>
2021-05-28 15:55:28 -0500

    update file structure

--- routes/api/index.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-const entryRoutes = require("./entries");
-const moodRoutes = require("./moods");
-
-// Entry routes
-router.use("/entries", entryRoutes);
-
-module.exports = router;
