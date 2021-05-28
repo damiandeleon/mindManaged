a9ee287939734c635674df2a725ccf0331438e75
Vanity <ktkyletran@gmail.com>
2021-05-28 16:30:45 -0500

    new file structure with client folder

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,5 @@
 const router = require("express").Router();
 const entryRoutes = require("./entries");
-const moodRoutes = require("./moods");
 
 // Entry routes
 router.use("/entries", entryRoutes);
