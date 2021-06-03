e7b5810316c45a65c9d61beae7db814ad4105a60
Vanity <ktkyletran@gmail.com>
2021-06-02 19:27:42 -0500

    heroku fix

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const path = require('path');
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
@@ -10,8 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  // app.use(express.static(__dirname + "client/public"));
-  app.use('/static', express.static(path.join(__dirname, 'client/public')));
+  app.use(express.static("client/build"));
 }
 // Add routes, both API and view
 app.use(routes);
