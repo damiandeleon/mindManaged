257c796fac5f216fad5b292581b63a317540468b
Damian De Leon <damianmdeleon@gmail.com>
2021-06-05 14:43:52 -0500

    ujournal page css feature

--- client/src/pages/Journal.js
+++ client/src/pages/Journal.js
@@ -1,21 +1,22 @@
 
 import React, { useState, useEffect } from "react";
-import { Form, Container, Row, Col, Card, Button } from "react-bootstrap";
+import { Form, Container, Row, Col, Card, CardDeck, Button, Jumbotron, Dropdown, DropdownButton, ListGroup } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import API from "../utils/API";
+import './Journal.css'
 
 
 const Journal = () => {
     let today = new Date();
     var DD = String(today.getDate()).padStart(2, '0');
-    var MM = String(today.getMonth() + 1).padStart(2, '0'); 
+    var MM = String(today.getMonth() + 1).padStart(2, '0');
     var YYYY = today.getFullYear();
     var HHMT = today.getHours();
     var HH = (today.getHours() + 24) % 12 || 12;
-    var mm = (today.getMinutes() < 10?'0':'') + today.getMinutes();
-    var ampm = HHMT >= 12? 'pm' : 'am';
-    
-    today = MM + '/' + DD + '/' + YYYY 
+    var mm = (today.getMinutes() < 10 ? '0' : '') + today.getMinutes();
+    var ampm = HHMT >= 12 ? 'pm' : 'am';
+
+    today = MM + '/' + DD + '/' + YYYY
     // + ' ' + HH + ':' + mm + ampm;
     let time = HH + ':' + mm + ampm;
 
@@ -47,10 +48,10 @@ const Journal = () => {
     };
 
     const handleTextChange = (event) => {
-     
+
         const { name, value } = event.target;
-        setFormObject({...formObject, [name]: value})
-      };
+        setFormObject({ ...formObject, [name]: value })
+    };
 
     const handleFormSubmit = (event) => {
         event.preventDefault();
@@ -61,33 +62,45 @@ const Journal = () => {
             date: today,
             time: time
         })
-        
-        .then(res=> loadEntries())
-        .catch(err => console.log(err));
+
+            .then(res => loadEntries())
+            .catch(err => console.log(err));
     }
 
     return (
         <Container fluid>
+
+
+
+            <Jumbotron id="jumbo-bg2">
+                <div className="text-center">
+                    <h1 style={{ fontSize: '75px' }} variant="info" id="jumbo-title">Your Journal</h1>
+                    <h4>A sanctuary for your thoughts</h4>
+                    <h6 >{today} </h6>
+                    <br></br>
+                </div>
+            </Jumbotron>
             <Row>
 
-                <Col md={7}>
-                    <div className="journalpageheader">
+                <Col md={9} >
+                {/* <Card.Img id="CardImg" fluid src="https://www.incimages.com/uploaded_files/image/1920x1080/getty_482405534_309336.jpg"/> */}
 
-                        <h2 value={today}>Today is: {today}</h2>
-                        <h3 value={time}>Time is: {time}</h3>
+                    <Form style={{background: 'lightcyan'}} onSubmit={handleFormSubmit}  >
+                        
+                        <CardDeck id="fade-in">
+                            
+                            <Card.Body style={{ textAlign: 'center' }}>
+                                
+                                <h1>Select Today's Mood</h1>
+                                
+                                <Form.Label>
 
-                    </div>
-                    <br />
-                    <br />
-                    <Form onSubmit={handleFormSubmit}  >
-                        <Card>
-                            <h1>Select Today's Mood</h1>
-                            <Form.Label>
-                                    <Form.Control 
+                                    <Form.Control
                                         as="select"
                                         onChange={handleTextChange}
                                         name="moodLevel"
-                                        >
+                                        className='primary'
+                                    >
                                         <option value={0}  >Select a Mood Level</option>
                                         <option value={5}  >5 Very Happy</option>
                                         <option value={4}  >4 Somewhat Happy</option>
@@ -96,44 +109,52 @@ const Journal = () => {
                                         <option value={1}  >1 Very Sad</option>
 
                                     </Form.Control>
-                            </Form.Label>
-                        </Card>
+                                </Form.Label>
+                            </Card.Body>
+                        </CardDeck>
 
-                        <Card>
-                            <h1>Today's Journal Entry</h1>
-                            <Form.Control
-                                as="textarea"
-                                rows={8}
-                                name="text"
-                                
-                                onChange={handleTextChange} 
-                            />
-                        </Card>
-                       
-                        <Button
-                         type="submit" value="Submit">Submit</Button>
+                        <CardDeck id="fade-in2" variant='top'>
+                            <Card.Body style={{  textAlign: 'center' }}>
+                                <h1 >Today's Journal Entry</h1>
+                                <Form.Control
+                                    as="textarea"
+                                    rows={8}
+                                    name="text"
+                                    onChange={handleTextChange}
+                                />
+                                <Button style={{ margin: '10px' }}
+                                    type="submit" value="Submit">Submit</Button>
+                            </Card.Body>
+                        </CardDeck>
+
+
+
                     </Form>
 
                     <br />
 
 
                 </Col>
-                <Col md={5}>
-                    <h1> Journal Entries </h1>
-                    {JournalEntry.length ?  (
-                        <ol>
-                            {JournalEntry.map(entry => (
-                                <li key={entry._id}>
-                                    <Link to={("/entries/" + entry._id)}>
-                                        <p> Entry Date: {entry.date + " " + entry.time} - Mood Level {entry.moodLevel}</p>
-                                    </Link>
-                                    <Button id="deleteButton" onClick={() => deleteEntry(entry._id)}>Delete Entry</Button>
-                                </li>
-                            ))}
-                        </ol>
-                    ) : (
-                        <h3>No Results to Display</h3>
-                    )}
+                <Col md={3}>
+                    <Card.Body id="fade-in3" style={{  textAlign: 'center', background: 'lightgrey'}}>
+                        <h1> Journal Entries </h1>
+                        {JournalEntry.length ? (
+                            <ListGroup>
+                                {JournalEntry.map(entry => (
+
+                                    <ListGroup.Item key={entry._id}>
+                                        <Link to={("/entries/" + entry._id)}>
+                                            <p style={{ fontSize: 'smallest' }} > Entry Date: {entry.date + " " + entry.time}</p>
+                                            <p style={{ fontSize: 'smallest' }} >  Mood Level {entry.moodLevel} </p>
+                                        </Link>
+                                        <Button size="sm" id="deleteButton" onClick={() => deleteEntry(entry._id)}>Delete</Button>
+                                    </ListGroup.Item>
+                                ))}
+                            </ListGroup>
+                        ) : (
+                            <h3>No Results to Display</h3>
+                        )}
+                    </Card.Body>
                 </Col>
             </Row>
         </Container>
