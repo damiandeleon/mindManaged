3418134fcbe1058d285b42bbd37faeb55fbc1df6
Caroline Miller <caroline.miller@ymail.com>
2021-06-11 13:29:34 -0500

    added intake code, controller, and model

---
+++ models/intake.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const intakeSchema = new Schema({
+    // user: { type: Schema.Types.ObjectId },
+    dates: { type: Array, required: true },
+});
+
+const Intake = mongoose.model("Intake", intakeSchema);
+
+module.exports = Intake;
